8e53c84a89ab85db95966b76763cc478780f6a51
luis <luisindriago202@gmail.com>
2022-12-26 22:35:04 -0400

    como mostrar el detalle de un post y como filtrar los posts de un blog por categoria

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::where('status',2)->latest('id')->paginate(8);
+        $posts = Post::where('stc atus',2)->latest('id')->paginate(8);
 
         return view('posts.index', compact('posts'));
+    }
+
+    public function show(Post $post ){
+
+        $related = Post::where( 'category_id', $post->category_id )
+                        ->where('status', 2)
+                        ->where('id', '!=', $post->id)
+                        ->latest('id')
+                        ->take(4)
+                        ->get();
+
+        return view('posts.show', compact('post', 'related'));
+    }
+
+
+
+    public function category( Category $category ){
+        
+        $posts = Post::where('category_id', $category->id)
+                        ->where('status', 2)
+                        ->latest('id')
+                        ->paginate(6);
+
+        return view('posts.category', compact( 'posts', 'category' ) );
     }
 }
